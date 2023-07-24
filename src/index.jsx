e47c3e17ea4a3c41f9d0633b464c53cac6fce18c
Shannon <shannon@Shannons-MacBook-Pro.local>
2023-07-24 06:08:09 -0500

    Add reset styling

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,7 @@
 import ReactDOM from 'react-dom/client';
 import Page from './global/Page';
 import Navigation from './global/Navigation';
-// import './styles/reset.css';
-
-// <Navigation />
+import './styles/reset.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <>
