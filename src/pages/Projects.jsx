c13f094ccec97719d7683192d7add6b85a0d653e
Shannon <shannon@Shannons-MacBook-Pro.local>
2023-07-26 15:46:12 -0500

    Remove unused style import

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,8 +1,6 @@
 import projects from '../lib/projectData';
 import ProjectCard from '../components/ProjectCard';
 
-import '../styles/article.css';
-
 const Projects = () => {
     return (
         <section aria-label='projects' id='projects'>
