d2056003f5e60ecf46eab7c6da406a86a5a9fc5e
Shannon <shannon@Shannons-MacBook-Pro.local>
2023-07-30 06:39:32 -0500

    Implement react strict mode

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,13 @@
 import ReactDOM from 'react-dom/client';
+import { StrictMode } from 'react';
 import Page from './global/Page';
 import Navigation from './global/Navigation';
 import './styles/reset.css';
 import './styles/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <>
+    <StrictMode>
         <Navigation />
         <Page />
-    </>
+    </StrictMode>
 );
