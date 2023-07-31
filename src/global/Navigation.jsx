fae2481d90c7cb5f0a42c675f2869c6f8de0acf4
Shannon <shannon@Shannons-MacBook-Pro.local>
2023-07-31 06:38:24 -0500

    Handle scroll when navigation links are clicked

--- src/global/Navigation.jsx
+++ src/global/Navigation.jsx
@@ -1,12 +1,26 @@
+import { useRef } from 'react';
+import { NavLink } from 'react-router-dom';
+
 const links = ['Home', 'About', 'Skills', 'Projects', 'Contact'];
 
 const Navigation = () => {
+    const anchorRefs = useRef([]);
+
+    function handleScroll(e) {
+        let target = anchorRefs.current.filter(anchor => anchor.id === e.target.hash.split('#')[1])[0];
+        if (target === undefined) {
+            target = document.querySelector(e.target.hash);
+            anchorRefs.current = [...anchorRefs.current, target];
+        }
+        target.scrollIntoView();
+    }
+
     return (
         <nav>
             <ul>
             {links.map(link => (
                 <li key={link}>
-                    <a href={'#' + link.toLowerCase()}>{link}</a>
+                    <NavLink to={{pathname: '/', hash: '#' + link.toLowerCase()}} onClick={handleScroll}>{link}</NavLink>
                 </li>
             ))}
             </ul>
